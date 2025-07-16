7610af192e0872abfe9c929e4af46d34ba5f28e5
Yusuke <yhakamaya@adobe.com>
2025-07-16 14:22:16 +0900

    add quote.js

---
+++ blocks/quote/quote.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+  const [quoteWrapper] = block.children;
+
+  const blockquote = document.createElement("blockquote");
+  blockquote.innerHTML = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
+}
