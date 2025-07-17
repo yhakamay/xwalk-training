f0998fe1f5d98ca14db3cc88776728f628ed6721
Yusuke <yhakamaya@adobe.com>
2025-07-17 11:11:18 +0900

    npm lint:fix

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
 
-  const blockquote = document.createElement("blockquote");
+  const blockquote = document.createElement('blockquote');
   blockquote.innerHTML = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
 }
